3ab15413a717f43b973d02ee50ddf9bdccd54588
NicoLaval <nicolas.laval@insee.fr>
2020-07-08 21:51:01 +0200

    Fix tests and minor issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { I18nextProvider } from "react-i18next";
+import App from "./component/App";
+import i18n from "./js/tools/i18n";
 import "./index.scss";
-import "./js/tools/i18n";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <I18nextProvider i18n={i18n}>
+    <App />
+  </I18nextProvider>,
+  document.getElementById("root")
+);
