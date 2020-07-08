2df8777a44e3729f9c778a72c7de86cd8e63a785
NicoLaval <nicolas.laval@insee.fr>
2020-07-08 22:04:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
-import App from "./component/App";
+import App from "./js/component/App";
 import i18n from "./js/tools/i18n";
 import "./index.scss";
 
